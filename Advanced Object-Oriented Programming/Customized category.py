38f7928aeef48ab1219579548147d4a41dd4f7a8
Allan.zhao <allan.zhao@agilex.ai>
2026-02-02 11:49:30 +0800

    fix:面向对象高级编程，错误、调试和测试、IO编程

---
+++ Advanced Object-Oriented Programming/Customized category.py
@@ -0,0 +1,62 @@
+class Student:
+    def __init__(self,name):
+        self.name = name
+
+
+class StudentwithStr:
+    def __init__(self,name):
+        self.name = name
+
+    def __str__(self):
+        return 'Student object (name: %s)' % self.name
+    
+class Fib(object):
+    def __init__(self):
+        self.a ,self.b = 0,1
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        if self.a > 100000:
+            raise StopIteration()
+        return self.a
+
+    # def __getitem__(self,n):
+    #     a, b = 1, 1
+    #     for x in range(n):
+    #         a, b = b, a + b
+    #     return a
+
+    def __getitem__(self,n):
+        if isinstance(n,int):
+            a, b = 1, 1
+            for x in range(n):
+                a, b = b, a + b
+            return a
+        if isinstance(n,slice):
+            start = n.start
+            stop = n.stop
+            a, b = 1, 1
+            L = []
+            for x in range(stop):
+                if x >= start:
+                    L.append(a)
+                a, b = b, a + b
+            return L
+    
+if __name__ == "__main__":
+    #print(Student('Bob'))
+    #print(StudentwithStr('Bob'))
+    # for n in Fib():
+    #     print(n)
+    # f = Fib()
+    # print(f[0])
+    # print(f[1])
+    # print(f[2])
+    # print(f[3])
+
+
+    f = Fib()
+    print(f[0:5])
